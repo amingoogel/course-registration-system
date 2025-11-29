b5d9ec32af8301fb9104b8ab0a9f2fe935d5df33
Amin Chenaneh <amingoogel35@gmail.com>
2025-11-29 18:40:33 +0330

    feat: custom user model with role

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),  # برای تست
-    path('api/', include('users.urls', namespace='users')),      # بعداً
-    path('api/', include('courses.urls', namespace='courses')),  # بعداً
+    # path('api-auth/', include('rest_framework.urls')),  # برای تست
+    # path('api/', include('users.urls', namespace='users')),      # بعداً
+    # path('api/', include('courses.urls', namespace='courses')),  # بعداً
 ]
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'مدیر'),
+        ('professor', 'استاد'),
+        ('student', 'دانشجو'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set', 
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set', 
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
