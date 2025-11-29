720ad13c9da14eefc498aa4e8773c3392fe06911
Amin Chenaneh <amingoogel35@gmail.com>
2025-11-29 20:42:23 +0330

    feat: Add path urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api-auth/', include('rest_framework.urls')),  # برای تست
-    # path('api/', include('users.urls', namespace='users')),      # بعداً
-    # path('api/', include('courses.urls', namespace='courses')),  # بعداً
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/courses/', include('courses.urls')),
 ]
---
+++ backend/courses/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CourseViewSet
+
+router = DefaultRouter()
+router.register(r'', CourseViewSet)
+
+urlpatterns = include(router.urls)
