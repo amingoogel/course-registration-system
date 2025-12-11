7c5db7b79312dab68f9b86c9abebb11022f1c87b
Amin Chenaneh <amingoogel35@gmail.com>
2025-12-11 22:45:38 +0330

    feat: Create url for Prerequisite

--- backend/courses/urls.py
+++ backend/courses/urls.py
@@ -4,6 +4,7 @@ from .views import CourseViewSet
 
 router = DefaultRouter()
 router.register(r'', CourseViewSet, basename='course') 
+router.register(r'prerequisites', PrerequisiteViewSet, basename='prerequisite')
 
 urlpatterns = [
     path('', include(router.urls)),  
