1ff3e95984f28b3e3812c6ad3ea0855306d4b183
Amin Chenaneh <amingoogel35@gmail.com>
2026-01-07 01:46:50 +0330

    feat: Create modle for selection

--- backend/selection/models.py
+++ backend/selection/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
+from courses.models import Course
 
-# Create your models here.
+class CourseSelection(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='selections', limit_choices_to={'role': 'student'})
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='selections')
+    selected_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('student', 'course')
+
+    def __str__(self):
+        return f"{self.student} - {self.course}"
