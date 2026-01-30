afce0bc8cfdad2a0c5b195b9213ec763819a1c51
Amin Chenaneh <amingoogel35@gmail.com>
2026-01-30 21:34:20 +0330

    feat: grade for every student courses

--- backend/selection/models.py
+++ backend/selection/models.py
@@ -13,3 +13,19 @@ class CourseSelection(models.Model):
 
     def __str__(self):
         return f"{self.student} - {self.course} {'(نهایی)' if self.is_finalized else ''}"
+        
+
+class Grade(models.Model):
+    selection = models.OneToOneField(CourseSelection, on_delete=models.CASCADE)
+    score = models.FloatField("نمره", validators=[MinValueValidator(0), MaxValueValidator(20)])
+    status = models.CharField("وضعیت", max_length=20, default="در حال بررسی")
+
+    def save(self, *args, **kwargs):
+        if self.score >= 10:
+            self.status = "قبول"
+        else:
+            self.status = "مردود"
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"نمره {self.selection.course} برای {self.selection.student}"
