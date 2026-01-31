f162fe5a3a347de31aac1a4cd79076372631b757
Amin Chenaneh <amingoogel35@gmail.com>
2026-01-31 23:49:42 +0330

    fix: login-history fixed

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,10 +3,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.urls import path, include
 from rest_framework import permissions
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+from users.auth import LoginHistoryTokenObtainPairView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -22,7 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     # JWT Login
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', LoginHistoryTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     # Courses API
