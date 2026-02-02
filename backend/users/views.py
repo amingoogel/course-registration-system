9f2b5d6f5502d72a2e3cd9b4cc2bdaac05e7363f
Amin Chenaneh <amingoogel35@gmail.com>
2026-02-03 03:02:37 +0330

    feat: two endpoints for list of all professors and students

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,7 +3,14 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from .models import User, LoginHistory
-from .serializers import RegisterStudentSerializer, RegisterProfessorSerializer, UserSerializer , LoginHistorySerializer
+from .serializers import (
+    RegisterStudentSerializer,
+    RegisterProfessorSerializer,
+    UserSerializer,
+    LoginHistorySerializer,
+    StudentListSerializer,
+    ProfessorListSerializer,
+)
 from rest_framework.views import APIView
 
 class IsAdminUser(IsAuthenticated):
@@ -38,15 +45,25 @@ class CurrentUserAPIView(APIView):
 
 
 class LoginHistoryViewSet(viewsets.ReadOnlyModelViewSet):
-
     serializer_class = LoginHistorySerializer
     permission_classes = [IsAuthenticated]
 
-
     def get_queryset(self):
         user = self.request.user
-        
         if user.role == 'admin':
             return LoginHistory.objects.all().order_by('-login_at')
-
         return LoginHistory.objects.filter(user=user).order_by('-login_at')
+
+
+class StudentListView(viewsets.ReadOnlyModelViewSet):
+    """لیست دانشجویان برای ادمین: شماره دانشجویی، اسم، فامیل"""
+    queryset = User.objects.filter(role='student').order_by('last_name', 'first_name')
+    serializer_class = StudentListSerializer
+    permission_classes = [IsAdminUser]
+
+
+class ProfessorListForAdminView(viewsets.ReadOnlyModelViewSet):
+    """لیست اساتید برای ادمین: شماره پرسنلی، اسم، فامیل"""
+    queryset = User.objects.filter(role='professor').order_by('last_name', 'first_name')
+    serializer_class = ProfessorListSerializer
+    permission_classes = [IsAdminUser]
